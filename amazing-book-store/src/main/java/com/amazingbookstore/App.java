2bd60bd3b6941a1cb0104b804e1e3dab54be7d78
Tiago <tiago.parnow@gmail.com>
2016-11-06 23:16:27 -0200

    Atualizando pastas

--- amazing-book-store/src/main/java/com/amazingbookstore/App.java
+++ amazing-book-store/src/main/java/com/amazingbookstore/App.java
@@ -1,5 +1,8 @@
 package com.amazingbookstore;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -7,5 +10,10 @@ package com.amazingbookstore;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        
+        EntityManagerFactory factory = Persistence.
+                createEntityManagerFactory("amazingbooksotreUnit");
+
+          factory.close();
     }
 }
